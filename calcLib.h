8aa9c9eabf92b12e09cfe733111c9bba419925f6
eusmhoc <sumanth.chodisetti@ericsson.com>
2025-10-21 23:04:16 +0530

    Added new required functions

--- calcLib.h
+++ calcLib.h
@@ -1,35 +1,30 @@
 #ifdef __cplusplus
-extern "C"{
+extern "C"
+{
 #endif
 
 #ifndef __CALC_LIB
 #define __CALC_LIB
 
-/* 
+  int initCalcLib(void);                   // Init internal variables to the library, if needed.
+  int initCalcLib_seed(unsigned int seed); // Init internal variables to the library, use <seed> for specific variable.
 
-This is the header file for the calcLib. It is a C library.
+  char *randomType(void);   // Return a string to a mathematical operator
+  int randomInt(void);      // Return a random integer, between 0 and 100.
+  double randomFloat(void); // Return a random float between 0.0 and 100.0
 
-If compiled with a C++ Compiler, the __cplusplus variable is defined, so when the PREPROCESSOR finds it, it adds the extern "C" { as to indicate to the compiler (next step) that this code is C, not C++. 
-
-The next item __CALC_LIB, is just a define to prevent issues if the header file is included from multiple files while the soultion is building. 
-
-Both ifdef/ifndef have corresponding endif's at the end, and in the correct sequence. 
-
-Implementation in calcLib.c
-
-*/
-  
-
-  int initCalcLib(void); // Init internal variables to the library, if needed. 
-  int initCalcLib_seed(unsigned int seed); // Init internal variables to the library, use <seed> for specific variable. 
-
-  char* randomType(void); // Return a string to an mathematical operator
-  int randomInt(void);// Return a random integer, between 0 and 100. 
-  double randomFloat(void);// Return a random float between 0.0 and 100.0
-
+  // Arithmetic functions
+  int add(int a, int b);               // Integer addition
+  int sub(int a, int b);               // Integer subtraction
+  int mul(int a, int b);               // Integer multiplication
+  int intDiv(int a, int b);            // Integer division (truncate)
+  double floatAdd(double a, double b); // Floating-point addition
+  double floatSub(double a, double b); // Floating-point subtraction
+  double floatMul(double a, double b); // Floating-point multiplication
+  double floatDiv(double a, double b); // Floating-point division
 
 #endif
 
 #ifdef __cplusplus
-}  
+}
 #endif
